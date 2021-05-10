df52d724c582a26c0997a94c7d567cd6407446f8
Petrov Kirill <ottokirik@gmail.com>
2021-05-11 01:32:33 +0300

    Реализовано: класс User, класс Eventing для обработки событий класса User

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello');
+import { User } from './models/User';
+
+/* const user = new User({ id: 1 });
+
+user.fetch();
+
+user.set({ name: 'Bob', age: 22 });
+user.save(); */
+
+/* const user2 = new User({ name: 'John', age: 25 });
+user2.save(); */
